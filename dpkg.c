a47483e076d85740627135fa6c8fd6c782ac50c9
xiangnian <xiangnian@uniontech.com>
2021-06-16 19:04:00 +0800

    do some change

--- dpkg.c
+++ dpkg.c
@@ -2,27 +2,31 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-
-#define DPKG_L "dpkg -L"
+#define MAX_LINE 500
+#define MAX_COLUMN 150
 
 int judge_file_exist(char *file_path);
 int judge_file_symlink(char *file_path );
+int judge_file_uid(char *file_path);
 
-char temp[100] = {0};
-char output[][50] = {0};
+
+char output[MAX_LINE][MAX_COLUMN] = {0};
 char debname[20];
 char exec_debname[20] = "dpkg -L ";
 
 int StorageStr(){
+    int i=0;
+    FILE *fp;
+    char temp[100] = {0};
     printf("please input the deb name:");
     scanf("%s",debname);
     strcat(exec_debname,debname);
-    
-    int i=0;
-    FILE *fp;
-    if ((fp = popen(exec_debname,"r")) == NULL)
-    {
+
+    if ((fp = popen(exec_debname,"r")) == NULL){
         perror("popen error\n");
         return -1;
     }
@@ -30,8 +34,7 @@ int StorageStr(){
     while (fgets(temp, 255, fp) != NULL)
     {
         memcpy(output[i],temp,strlen(temp)-1);
-        //printf("%s\n",output[i]);
-        judge_file_exist(output[i]);
+        judge_file_uid(output[i]);
         judge_file_symlink(output[i]);
         memset(temp,0,sizeof(temp));
         i++;
@@ -45,33 +48,29 @@ int StorageStr(){
     
 }
 
-
+// 判断文件路径以及文件是否存在,已经弃用
 int judge_file_exist( char *file_path)   
 {   
     char *temp=file_path;
     if((access(temp,F_OK)) != 0){   
         printf("file %s not exist\n",temp);  
     }
-
-
     return 0;   
 }
 
+// 通过sudo file file_patch 方式判断符号链接是否正常
 int judge_file_symlink(char *file_path){
     char file_name[100] = "sudo file ";
     FILE *fp;
     char temp[500] = {0};
     strcat(file_name,file_path);
-    // printf("AA%s\n",file_name);
-    if ((fp = popen(file_name,"r")) == NULL)
-    {
+    if ((fp = popen(file_name,"r")) == NULL){
         perror("popen error\n");
         return -1;
     }
     fgets(temp, 255, fp);
-    // printf("%s\n",temp);
     if(strstr(temp,"broken symbolic link")){
-        printf("broken symbolic link\n");
+        printf("%s\n",file_path);
     }
     memset(file_name,0,sizeof(file_name));
     if (pclose(fp) == -1) {
@@ -81,7 +80,29 @@ int judge_file_symlink(char *file_path){
     return 0;
 }
 
+// 判断文件数组是否正常
+int judge_file_uid(char *file_path){
+    
+    struct stat stat_data;
+
+    // stat函数 可以判断文件是否存在
+    if (0 != stat(file_path, &stat_data)) {
+        printf("%s\n",file_path);
+        return -1;
+    }
+    // 仅仅判断 /usr 文件属主
+    if (!strstr(file_path,"/usr/")){
+        return 0;
+    }
+
+    if (stat_data.st_uid != 0){
+        printf("%s",file_path);
+    }
+    
+    return 0;
+}
 
 int main(){
     StorageStr();
+    return 0;
 }
